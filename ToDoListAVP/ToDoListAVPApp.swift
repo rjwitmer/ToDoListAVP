153ac112200bb7e937cd3ada0de649be62659c54
Bob Witmer <bwitmer@golden.net>
2025-08-19 14:07:24 -0400

    Initial Commit

---
+++ ToDoListAVP/ToDoListAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAVPApp.swift
+//  ToDoListAVP
+//
+//  Created by Bob Witmer on 2025-08-19.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
